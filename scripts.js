0dc42a8ff74c3611887068526adc6e43b5e2bc06
stefy <stefy.mp15@gmail.com>
2021-02-08 21:29:24 -0300

    desafios

--- scripts.js
+++ scripts.js
@@ -15,7 +15,32 @@ window.onload = function () {
   let edadUser = prompt("¿Cuántos años tenés?");
   let generoUser = prompt("¿Cuál es tu género?");
 
-  var myUser = new Usuario(nombreUser, apellidoUser, edadUser, generoUser);
+  let myUser = new Usuario(nombreUser, apellidoUser, edadUser, generoUser);
   console.log(myUser);
   myUser.saludar();
+
+  let hi = document.querySelector(".username");
+  hi.value = nombreUser + " " + apellidoUser;
 };
+
+function Producto(nombre, talle, modelo, precio) {
+  this.nombre = nombre;
+  this.talle = talle;
+  this.modelo = modelo;
+  this.precio = precio;
+}
+
+let remeraBasic = new Producto("basica", "L", "2021", 600);
+let pantalonBasic = new Producto("jean", "L", "2020", 1500);
+let calzadoBasic = new Producto("zapatillas", "39", "2021", 1000);
+
+let productos = [remeraBasic, pantalonBasic, calzadoBasic];
+console.log(productos);
+
+let despedida = document.querySelector(".boton-adios");
+despedida.addEventListener("click", saludar);
+
+function saludar() {
+  alert("Gracias por su compra");
+  despedida.removeEventListener("click", saludar);
+}
